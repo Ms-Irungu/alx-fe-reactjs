d46cf27b79ef874fe7b77f67921c80043677dd71
Valentine Irungu <irunguvalentine73@gmail.com>
2025-02-20 12:07:56 +0300

    added UserProfile.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,15 +6,19 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 function App () {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div>
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </div>
       <div>
         <a href='https://vite.dev' target='_blank'>
           <img src={viteLogo} className='logo' alt='Vite logo' />
