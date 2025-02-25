4c83559d564e51d9447dc801fdfa3b68899ea6bf
Valentine Irungu <irunguvalentine73@gmail.com>
2025-02-25 11:33:18 +0300

    added inline css styling to the React Components

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 export default function MainContent () {
     return (
         <main>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
+            <p style = {{color: 'hotpink', fontSize: '20px'}}>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
 }
--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 function UserProfile(props) {
     return (
-        <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
+        <div style = {{border: '1px solid gray', margin: '10px', padding: '10px'}}>
+            <h2 style={{color: 'blue'}}>{props.name}</h2>
+            <p>Age: <span style={{fontWeight: 'bold'}}>{props.age}</span></p>
             <p>Bio: {props.bio}</p>
         </div>
     );
