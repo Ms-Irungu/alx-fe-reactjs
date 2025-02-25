c8cdbadeaa55fce5805d6aa757afdbb7cdf60b79
Valentine Irungu <irunguvalentine73@gmail.com>
2025-02-25 12:22:44 +0300

    reversed h1 tag back to the p tag

--- alx-react-app-new/src/components/counter.jsx
+++ alx-react-app-new/src/components/counter.jsx
@@ -4,7 +4,7 @@ function Counter() {
     const [count, setCount] = useState(0);
     return (
         <div>
-            <h1>Current Count: {count}</h1>
+            <p>Current Count: {count}</p>
             <button onClick={() => setCount(count + 1)}>Increment</button>
             <button onClick={() => setCount(count - 1)}>Decrement</button>
             <button onClick={() => setCount(0)}>Reset</button>
