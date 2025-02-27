c3e380b1330277448ff2e1a5fad052a895873d8c
Valentine Irungu <irunguvalentine73@gmail.com>
2025-02-28 00:42:44 +0300

    added userData info on App.jsx file

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,7 +3,7 @@ import UserContext from './components/UserContext';
 
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com", age: 25, bio: "Hello, my name is Jane Doe. I am a software developer." };
 
   return (
   <UserContext.Provider value={userData}>
