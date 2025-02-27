9e0cb189251c9767d3e53ef89ee28ebd2697e1af
Valentine Irungu <irunguvalentine73@gmail.com>
2025-02-28 00:31:39 +0300

    Added context API to UserContext file

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types';
-function UserProfile(props) {
+import { useContext } from 'react';
+import UserContext from './components/UserContext';
+
+function UserProfile() {
+    const userData = useContext(UserContext);
+
     return (
         <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio}</p>
+            <h2>{userData.name}</h2>
+            <p>Age: {userData.age}</p>
+            <p>Bio: {userData.bio}</p>
         </div>
     );
 };
