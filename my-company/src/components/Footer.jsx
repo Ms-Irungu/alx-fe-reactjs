291956a53e9e912e605cd83ffff7321d1418673e
Valentine Irungu <irunguvalentine73@gmail.com>
2025-03-01 15:59:32 +0300

    Implemented Route Router on my-company app

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+    return (
+      <footer style={{ textAlign: 'center', padding: '10px', backgroundColor: '#333', color: 'white' }}>
+        <p>© 2025 My Company</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
