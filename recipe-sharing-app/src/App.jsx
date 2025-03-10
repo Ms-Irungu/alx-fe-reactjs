cadf582ee2b767c24a32e19779cccd624731271a
Valentine Irungu <irunguvalentine73@gmail.com>
2025-03-10 16:48:50 +0300

    changed Component to components

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,5 @@
-import RecipeList from "./Component/RecipeList" 
-import AddRecipeForm from "./Component/AddRecipeForm"
+import RecipeList from "./components/RecipeList" 
+import AddRecipeForm from "./components/AddRecipeForm"
 
 function App() {
 
