016297a56844f4faf5b3c27cc90b286412a1f848
Valentine Irungu <irunguvalentine73@gmail.com>
2025-03-14 05:47:32 +0300

    used @tanstack/react-query instead of react-query

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import {QueryClient, QueryClientProvider} from 'react-query'
-import {ReactQueryDevtools} from 'react-query/devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import PostsComponent from './components/PostsComponent'
 
 const queryClient = new QueryClient();
 
--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,44 +1,38 @@
 import React from 'react'
-import {useQuery} from 'react-query'
-
-
-//Define a fetch function that can be used to fetch data from an API
-const fetchPosts = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-  return response.json()
-}
-console.log(fetchPosts())
+import { useQuery } from '@tanstack/react-query';
 
 //Define a PostsComponent that will use the useQuery hook to fetch data from the API
 const PostsComponent = () => {
 
     //Use the useQuery hook to fetch data from the API
-    const {data, error, isLoading} = useQuery('FetchData', fetchPosts)
+    const { data, error, isLoading } = useQuery({
+        queryKey: ['posts'],
+        queryFn: () =>
+            fetch('https://jsonplaceholder.typicode.com/posts')
+                .then((res) => res.json()),
+    });
 
     //If the data is still loading, display a loading message
     //Handle loading state
-    if (isLoading) {
-        return <div>Loading...</div>
-    }
+    if (isLoading) return <div>Loading...</div>
+
     //If there is an error fetching the data, display an error message
     //Handle error state
-    if (error) {
-        return <div>Error loading data</div>
-    }
+    if (error) return <div>Error loading data</div>
 
     //If the data has been fetched successfully, display the data
     //Handle success state
     //Render the fetched data
-  return (
-    <div>
-        <h1>Posts</h1>
-        <ul>
-            {data.map(post => (
-            <li key={post.id}>{post.title}</li>
-            ))}
-        </ul>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Posts</h1>
+            <ul>
+                {data.map(post => (
+                    <li key={post.id}>{post.title}</li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default PostsComponent;
