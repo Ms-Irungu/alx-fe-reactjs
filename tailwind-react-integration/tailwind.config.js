36bf117e5b238b4b4b5042066c501011d70ad421
Valentine Irungu <irunguvalentine73@gmail.com>
2025-03-27 10:16:03 +0300

    add darkMode to tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
   content: [],
   theme: {
     extend: {},
