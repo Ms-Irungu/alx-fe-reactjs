dac3ef330d5cbfb3c89710b592e07833adb32014
Valentine Irungu <irunguvalentine73@gmail.com>
2025-03-28 06:36:33 +0300

    Create recipe-sharing-platform app

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Recipe Sharing Platform</h1>
+      <p>Share your favorite recipes with the world!</p>
+    </>
+  )
+}
+
+export default App
