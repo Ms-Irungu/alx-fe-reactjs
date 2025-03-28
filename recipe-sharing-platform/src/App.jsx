229c278fb18e8a2eaa48f004c2ba3765d8287481
Valentine Irungu <irunguvalentine73@gmail.com>
2025-03-28 23:53:21 +0300

    Add header part

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -4,7 +4,15 @@ import HomePage from './components/HomePage'
 function App () {
   return (
     <>
-      <h1 className="font-bold text-2xl" >Welcome to my Recipe Sharing Platform</h1>
+      <section className='text-center mb-8 pt-8' >
+        <h2 className='text-3xl font-bold text-gray-900 mb-4'>
+          Discover Delicious Recipes
+        </h2>
+        <p className='text-gray-600 max-w-2xl mx-auto'>
+          Explore our collection of mouth-watering recipes shared by food
+          enthusiasts from around the world.
+        </p>
+      </section>
       <div>
         <HomePage />
       </div>
