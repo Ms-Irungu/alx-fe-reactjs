0dd30f046645cb235b241170134b9ef496872a3d
Valentine Irungu <irunguvalentine73@gmail.com>
2025-03-28 05:55:21 +0300

    Add Responsive Inage Sizing

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile() {
     return (
       <div className="bg-gray-100 sm:p-4 sm:max-w-xs md:p-8 md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-        <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 mx-auto" />
+        <img src="https://via.placeholder.com/150" alt="User" className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto" />
         <h1 className= "sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
         <p className="text-gray-600 md:text-base sm:text-sm" >Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
