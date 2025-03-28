9390ba0aa5d16837ab2900d4370e21352499c6df
Valentine Irungu <irunguvalentine73@gmail.com>
2025-03-28 22:31:45 +0300

    Implement a Responsive Grid

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -18,7 +18,7 @@ const HomePage = () => {
   }, []) //fetch recipes from the API
 
   return (
-    <section className='flex flex-wrap justify-center gap-8'>
+    <section className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 p-5'>
         {error && <p className="text-red-500">{error}</p>} {/* Show error if fetching fails */}
       
         {recipes.map(recipe => (
