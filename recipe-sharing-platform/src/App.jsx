1f1a248a54bdb1b70ec839323327896c322008b3
Valentine Irungu <irunguvalentine73@gmail.com>
2025-03-29 12:17:01 +0300

    Add Routes to HomePage and RecipeDetail

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './components/HomePage'
+import RecipeDetail from './components/RecipeDetail';
 
 function App () {
   return (
     <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/recipe/:id' element={<RecipeDetail />} />
+      </Routes>
+    </Router>
+    
       <section className='text-center mb-8 pt-8' >
         <h2 className='text-3xl font-bold text-gray-900 mb-4'>
           Discover Delicious Recipes
